2f1ccf1e94623d7e2e06f7e72a2ccb9fd836d06b
Hank Lee <hank999887@gmail.com>
2016-11-29 02:45:36 +0800

    [ADD] function: save_to_file [ADD] .gitignore file

--- src/lib/scraping_helper.py
+++ src/lib/scraping_helper.py
@@ -19,5 +19,14 @@ def parse_indices(html_bundle):
 
 
 def save_to_file(idx_name, page_num, indices_list):
-    pass
+    file_name = idx_name + "_" + page_num
+    root_dir_path = op.abspath(op.join(__file__, op.pardir, op.pardir, op.pardir))
+    scraped_files_dir = op.join(root_dir_path, "indices")
+    target_dir = op.join(scraped_files_dir, idx_name)
+    if not op.exists(target_dir):
+        os.mkdir(target_dir)
+    with open(op.join(target_dir, file_name), "w") as f:
+        for index in indices_list:
+            f.write(index)
+            f.write("\n")
 
